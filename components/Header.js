62aab84c926cf46de7784bc24daab2bbf8f06265
Alexandre Chauvin <alexandre.chauvin@epitech.eu>
2023-03-19 18:39:19 +0100

    add: app header

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Box, Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Box
+      align="center"
+      p={6}
+      bgGradient="linear(to-r, #7928CA, #FF0080)"
+      rounded="lg"
+    >
+      <Text fontSize="4xl" color="white">
+        Your Book Creator
+      </Text>
+    </Box>
+  );
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import { Box } from "@chakra-ui/react";
 
 // Components
 import Book from "../../components/Book";
+import Header from "../../components/Header";
 
 export default function Home() {
   return (
     <>
       <Box p={50}>
+        <Header />
         <Book />
       </Box>
     </>
